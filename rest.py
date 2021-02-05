ef89ef73ce60cf152beca722199f983f7b51d4a8
Mike Holzinger <mike.holzinger@gmail.com>
2021-02-05 14:04:57 -0800

    Adds character ID and search from Marvel API

--- rest.py
+++ rest.py
@@ -4,6 +4,12 @@ import json, requests
 from time import time
 from hashlib import md5
 
+# Static constants
+marvel_endpoint = 'https://gateway.marvel.com:443/v1/public/'
+character_name = "Ajaxis"
+#response = requests.get(url, params=auth).json()
+#print(response)
+
 def get_apikeys():
   """
   Load Marvel account api keys from text file
@@ -18,25 +24,64 @@ def get_apikeys():
 
 def marvel_auth(keychain):
   """
-  Construct authentication hash
+  Build marvel auth dictionary
   :param keychain: dict
   :return: dict
   """
   authentication = {}
+
   unix_timestamp = int(time())
-  plaintext = str(unix_timestamp) + keychain["MARVEL_PRIVKEY"] + keychain["MARVEL_PUBKEY"]
+  plaintext = str(unix_timestamp) + \
+    keychain["MARVEL_PRIVKEY"] + keychain["MARVEL_PUBKEY"]
   md5_ciphertext = md5(plaintext.encode("utf-8")).hexdigest()
   authentication['ts'] = str(unix_timestamp)
   authentication['apikey'] = keychain["MARVEL_PUBKEY"]
   authentication['hash'] = md5_ciphertext
+
   return authentication
 
-marvel_endpoint = 'https://gateway.marvel.com:443/v1/public/'
+def find_character_id(name):
+  """
+  Search for Marvel character from the developer API
+  :param character_string: str
+  :return: str
+  """
+  url = (marvel_endpoint + 'characters')
+  character = requests.get(url, params=auth).json()
+   # Write some pagination magic, this is vital
+  for item in character['data']['results']:
+    if name in item['name']:
+      return item['id']
+
+def get_char_data(id):
+  """
+  Query for char data found in the Marvel Developer API
+  :param character_id: str
+  :return: dict
+  """
+  dossier = {}
+
+  image_size = "standard_fantastic"
+  url = (marvel_endpoint + 'characters/' + str(id))
+  result = requests.get(url, params=auth).json()
+
+  dossier['name'] = result['data']['results'][0]['name']
+  dossier['id'] = result['data']['results'][0]['id']
+  dossier['description'] = result['data']['results'][0]['description']
+
+  # Image path formattiing here:
+  # https://developer.marvel.com/documentation/images
+  image_path = result['data']['results'][0]['thumbnail']['path']
+  extension = result['data']['results'][0]['thumbnail']['extension']
+  dossier['jpeg'] = image_path + '/' + image_size + '.' + extension
+
+  return dossier
 
 # Load keys in dictionary and then next step
 keychain = get_apikeys()
 auth = marvel_auth(keychain)
-url = (marvel_endpoint + 'characters')
 
-response = requests.get(url, params=auth)
-print(response.text)
+# Find character by name
+id = find_character_id(character_name)
+dossier = get_char_data(id)
+print (dossier)
